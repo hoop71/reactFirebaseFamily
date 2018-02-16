b5966b9e304c39f23c85cd6fcb685de1adf2c4e1
Matt <mhooper72@gmail.com>
2018-02-16 16:15:42 -0700

    first JSON app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,59 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { database } from "./firebase";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      newData: ""
+    };
+
+    this.dataRef = null;
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.dataRef = database.ref("/matt/hooper/things");
+    this.dataRef.on("value", snapshot => {
+      this.setState({
+        data: snapshot.val()
+      });
+    });
+  }
+
+  handleChange(event) {
+    const newData = event.target.value;
+    this.setState({
+      newData: newData
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.dataRef.push(this.state.newData);
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App--header">
-          <h2>Welcome to React and Firebase</h2>
+          <h2>Matt Does Firebase and React</h2>
         </div>
         <pre className="App--data">
-          One day, some data from Firebase will go here.
+          {JSON.stringify(this.state.data, null, 2)}
         </pre>
+        <form
+          className="App--form"
+          value={this.state.newData}
+          onSubmit={this.handleSubmit}
+        >
+          <input type="text" onChange={this.handleChange} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const config = {
+	apiKey: "AIzaSyBF7GCoUSa8BcDTE3yIqfgZWsW_s6GwPN8",
+	authDomain: "fcreact888.firebaseapp.com",
+	databaseURL: "https://fcreact888.firebaseio.com",
+	projectId: "fcreact888",
+	storageBucket: "fcreact888.appspot.com",
+	messagingSenderId: "666473387630"
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
+
+export const database = firebase.database();
